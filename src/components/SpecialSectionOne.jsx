84898c4e03b0f6483d76a5a7650eb19c9b227698
Ferdous Ahmed Himel <himibaba10@gmail.com>
2025-01-09 00:51:18 +0600

    Fixed some css

--- src/components/SpecialSectionOne.jsx
+++ src/components/SpecialSectionOne.jsx
@@ -1,35 +1,44 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import image1 from "../assets/collection-item1.jpg"
-import image2 from "../assets/collection-item2.jpg"
-import image3 from "../assets/collection-item3.jpg"
-import './SpecialSectionOne.css'
+import image1 from "../assets/collection-item1.jpg";
+import image2 from "../assets/collection-item2.jpg";
+import image3 from "../assets/collection-item3.jpg";
+import "./SpecialSectionOne.css";
 
-
-const SpecialSectionOne = props => {
-    return (
-        <div className='mt-30'>
-            <div className='flex gap-4'>
-                <div>
-                    <p className='casual'>Casual Collection</p>
-                    <h2 className='text-5xl font-extrabold street'>Street <br /> Wear</h2>
-                    <img className='mt-20 rounded-lg' src={image1} alt="" />
-                </div>
-
-                <div>
-                    <h2 className='text-5xl font-extrabold shoe1'>25% off Basic <br /> Shoes</h2>
-                    <img className='mt-20' src={image2} alt="" />
-                    <h2 className='text-5xl font-extrabold shoe2'>Woolen <br /> Hat</h2>
-                    <img className='mt-8' src={image3} alt="" />
-
-                </div>
-            </div>
+const SpecialSectionOne = () => {
+  return (
+    <div className="mt-30">
+      <div className="flex gap-4">
+        {/* Left side */}
+        <div className="relative mt-20">
+          <p className="casual">Casual Collection</p>
+          <h2 className="text-5xl font-extrabold absolute right-5 top-5">
+            Street <br /> Wear
+          </h2>
+          <img className="rounded-lg" src={image1} alt="" />
         </div>
-    );
+
+        {/* right side */}
+        <div>
+          {/* Top card */}
+          <div className="relative mt-20">
+            <h2 className="text-3xl font-extrabold absolute right-5 bottom-5">
+              25% off Basic <br /> Shoes
+            </h2>
+            <img src={image2} alt="" />
+          </div>
+
+          {/* Bottom card */}
+          <div className="relative mt-8">
+            <h2 className="text-3xl lg:text-5xl font-extrabold absolute left-5 top-5">
+              Woolen <br /> Hat
+            </h2>
+            <img src={image3} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-SpecialSectionOne.propTypes = {
-    
-};
+SpecialSectionOne.propTypes = {};
 
 export default SpecialSectionOne;
--- src/components/SpecialSectionTwo.jsx
+++ src/components/SpecialSectionTwo.jsx
@@ -1,44 +1,42 @@
-import React from "react";
-import PropTypes from "prop-types";
 import image1 from "../assets/post-img1.jpg";
 import image2 from "../assets/post-img2.jpg";
 import image3 from "../assets/post-img3.jpg";
 
-const SpecialSectionTwo = (props) => {
+const SpecialSectionTwo = () => {
   return (
     <div>
-      <div className="grid grid-cols-3 mt-20 mx-auto">
-        <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="grid grid-cols-3 mt-20 mx-auto gap-10">
+        <div className="p-3 lg:p-5 xl:p-8 w-full bg-base-100 shadow-xl">
           <figure className="px-10 pt-10">
-            <img
-              src={image1}
-              alt="Shoes"
-              className="rounded-xl"
-            />
+            <img src={image1} alt="Shoes" className="rounded-xl" />
           </figure>
           <div className="card-body items-center text-center">
-            <h2 className="card-title">Top 10 Casual Look Ideas To Dress Up Your Kids</h2>
-            
+            <h2 className="card-title">
+              Top 10 Casual Look Ideas To Dress Up Your Kids
+            </h2>
           </div>
         </div>
-        <div className="card w-96 bg-base-100 shadow-xl">
-  <figure className="px-10 pt-10">
-    <img src={image2} alt="Shoes" className="rounded-xl" />
-  </figure>
-  <div className="card-body items-center text-center">
-    <h2 className="card-title">Latest Trends Of Wearing Street Wears Supremely</h2>
-    
-  </div>
-</div>
-<div className="card w-96 bg-base-100 shadow-xl">
-  <figure className="px-10 pt-10">
-    <img src={image3} alt="Shoes" className="rounded-xl" />
-  </figure>
-  <div className="card-body items-center text-center">
-    <h2 className="card-title">Types Of Comfortable Clothes Ideas For Women</h2>
-    <p></p>
-  </div>
-</div>
+        <div className="p-3 lg:p-5 xl:p-8 w-full bg-base-100 shadow-xl">
+          <figure className="px-10 pt-10">
+            <img src={image2} alt="Shoes" className="rounded-xl" />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">
+              Latest Trends Of Wearing Street Wears Supremely
+            </h2>
+          </div>
+        </div>
+        <div className="p-3 lg:p-5 xl:p-8 w-full bg-base-100 shadow-xl">
+          <figure className="px-10 pt-10">
+            <img src={image3} alt="Shoes" className="rounded-xl" />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">
+              Types Of Comfortable Clothes Ideas For Women
+            </h2>
+            <p></p>
+          </div>
+        </div>
       </div>
     </div>
   );
